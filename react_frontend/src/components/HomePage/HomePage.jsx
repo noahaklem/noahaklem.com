b8fa41cf843ede0764877c99f707b721448eddbc
Noah A Klem <noahaklem@gmail.com>
2024-01-02 13:39:15 -0700

    home page style

--- react_frontend/src/components/HomePage/HomePage.jsx
+++ react_frontend/src/components/HomePage/HomePage.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import ProfileCard from '../ProfileCard/ProfileCard'
+import './HomePage.css'
+import profilePicture from '../../assets/profilePicture.jpg'
 
 const HomePage = () => {
   return (
-    <div>Home Page</div>
+    <div className='home-page'>
+        <section className='home-page-hero'>
+            <h1>Tagline for profile.</h1>
+            <img src={profilePicture} />
+        </section>
+        <section className='home-page-cards-container'>
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+        </section>
+    </div>
   )
 }
 
