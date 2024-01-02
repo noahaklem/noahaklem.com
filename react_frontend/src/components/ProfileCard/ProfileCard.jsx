e3160ef10b92c3ccc23bd79a84294ea4caf3f000
Noah A Klem <noahaklem@gmail.com>
2024-01-02 13:39:43 -0700

    profile add button and position

---
+++ react_frontend/src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProfileCard = () => {
+  return (
+    <div>
+        <h3>Profile One</h3>
+        <img />
+        <button>Select</button>
+    </div>
+  )
+}
+
+export default ProfileCard
