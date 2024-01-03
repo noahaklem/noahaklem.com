dd3f35174ca13da492177f25b80d8db29f9250f9
Noah A Klem <noahaklem@gmail.com>
2024-01-03 12:57:50 -0700

    delete node modules

--- react_frontend/src/components/Profile/Profile.jsx
+++ react_frontend/src/components/Profile/Profile.jsx
@@ -1,17 +1,44 @@
 import React from 'react'
 import Navigation from './ProfileComponents/Navigation/Navigation'
-import About from './ProfileComponents/About/About'
-import Technologies from './ProfileComponents/Technologies/Technologies'
-import Testimonials from './ProfileComponents/Testimonials/Testimonials'
+import Card from './ProfileComponents/Card/Card'
 import Projects from './ProfileComponents/Projects/Projects'
+import SmallPicture from "../../assets/SmallProfilePicture.jpg"
 
 const Profile = () => {
+    const data = {
+        about: {
+            title: "ABOUT",
+            description: "Put some stuff in here about me",
+            imgs: [SmallPicture]
+        },
+        technologies: {
+            title: "TECHNOLOGIES",
+            description: "Put some stuff in here about me",
+            imgs: [SmallPicture, SmallPicture, SmallPicture]
+        },
+        testimonials: {
+            title: "TESTIMONIALS",
+            description: "Put some stuff in here about me",
+            imgs: [SmallPicture, SmallPicture, SmallPicture, SmallPicture, SmallPicture, SmallPicture, SmallPicture ]
+        }
+    }
+
+    const renderProfileCards = (data) => {
+        const keys = Object.keys(data)
+        return keys.map( key => {
+            return (
+                <Card 
+                    title={data[key].title}
+                    description={data[key].description}
+                    imgs={data[key].imgs}
+                />
+            )
+        })
+    }
   return (
     <div className='profile'>
         <Navigation />
-        <About />
-        <Technologies />
-        <Testimonials />
+        { renderProfileCards(data) }
         <Projects />
     </div>
   )
--- react_frontend/src/components/Profile/ProfileComponents/Projects/Projects.jsx
+++ react_frontend/src/components/Profile/ProfileComponents/Projects/Projects.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import "./Projects.css"
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className='projects'>
+        <div className='projects-container'>
+            <div className='projects-title-container'>
+                <h3>Projects</h3>
+            </div>
+            <div className='project-description-container'>
+                <p>Detailed description of project</p>
+            </div>
+            <div className='project-image-container'>
+                <img src='' alt="project one"/>
+                <img src='' alt="project two"/>
+                <img src='' alt="project three"/>
+            </div>
+        </div>
+        <div className='selected-project-container'>
+            <div className='selected-project-image-container'>
+                <img src='' alt='selected project image' />
+            </div>
+            <div className='selected-project-title-container'>
+                <h4>Project Title</h4>
+            </div>
+            <div className='selected-project-description-container'>
+                <p>Scope of the project here</p>
+            </div>
+            <div className='selected-project-technologies-container'>
+                <h5>Technology</h5>
+                <ul>
+                    <li>tech one</li>
+                    <li>tech two</li>
+                    <li>tech three</li>
+                    <li>tech four</li>
+                    <li>tech five</li>
+                </ul>
+            </div>
+        </div>
+    </div>
   )
 }
 
