d6d4483dc701ec15a372aefef2a510b7e52a68a4
Noah A Klem <noahaklem@gmail.com>
2024-01-09 13:48:38 -0700

    change location of entry point/ application controller

--- ruby_backend/app/controllers/api/v1/profile_controller.rb
+++ ruby_backend/app/controllers/api/v1/profile_controller.rb
@@ -1,7 +1,6 @@
 module Api
     module V1
         class ProfileController < ApplicationController
-
             get '/api/v1/:name' do
                 profile = Profile.find_by(name: params[:name])
                 if profile
@@ -10,11 +9,35 @@ module Api
                             id: p.id,
                             name: p.name,
                             url_path: p.url_path,
+                            tagline: p.tagline,
+                            about: p.about,
                             images: profile.images.map do |image|
                                 {
                                     id: image.id,
                                     image_data: Base64.strict_encode64(image.image_data),
                                     description: image.description
+                                }
+                            end,
+                            projects: profile.projects.map do |project|
+                                {
+                                    id: project.id,
+                                    scope: project.scope,
+                                    description: project.description,
+                                    images: project.images.map do |image|
+                                        {
+                                            id: image.id,
+                                            image_data: Base64.strict_encode64(image.image_data),
+                                            description: image.description,
+                                        }
+                                    end,
+                                }
+                            end,
+                            testimonials: profile.testimonials.map do |testimony|
+                                {
+                                    id: testimony.id,
+                                    review: testimony.review,
+                                    reviewer: testimony.reviewer,
+                                    image: testimony.image,
                                 }
                             end,
                             created_at: p.created_at,
@@ -28,6 +51,21 @@ module Api
                 end
 
                 content_type :json 
+                json_response.to_json
+            end
+
+            get '/api/v1/:name/projects' do
+                profile = Profile.find_by(name: params[:name])
+                if profile
+                    json_response = {
+                        message: "We found it but this isn't build quite yet!"
+                    }
+                else
+                    json_response = {
+                        message: "Hmm, I couldn't find that profile."
+                    }
+                end
+                content_type :json
                 json_response.to_json
             end
         end
